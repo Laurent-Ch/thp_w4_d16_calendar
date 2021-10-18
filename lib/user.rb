b20c6cf405c28a2ab956232dd7dbe53d8b56d80d
Laurent-Ch <chamouleau.laurent@gmail.com>
2021-10-18 10:18:44 +0200

    User class prototype

--- lib/user.rb
+++ lib/user.rb
@@ -1,3 +1,20 @@
 class User
+  attr_accessor :email, :age
+  @@all_users = []
 
+  def initialize(user_name, user_email, user_age)
+    @name = user_name
+    @email = user_email
+    @age = user_age
+    
+    # Putting user variables in a array and storing it within a class variable.
+    @user_data = [@name, @email, @age]
+    @@all_users << @user_data
+    #@@all_users << self.name
+  end
+  
+  # Pour l'appeler, User.all
+  def self.all
+    return @@all_users
+  end
 end
